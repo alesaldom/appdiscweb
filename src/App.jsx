4334f2df267c363070b51b76a87121248bdf8ba9
Alexandretta <alxsalazar21@gmail.com>
2025-04-08 16:14:25 -0500

    Initial project boilerplat

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './style/App.css'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import Test from './pages/Test'
+
+function App() {
+
+  return (
+    <>
+      <Register />
+      <Login />
+      <Test />
+    </>
+  )
+}
+
+export default App
